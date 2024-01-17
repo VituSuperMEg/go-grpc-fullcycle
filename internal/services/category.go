6d9e4448bf216d6ef52ecd0b5bfad2b9eb684557
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-17 19:14:14 -0300

    implement catecategoryResponse

--- internal/services/category.go
+++ internal/services/category.go
@@ -19,9 +19,16 @@ func NewCategoryService(categoryDB database.Category) *CategoryService {
 }
 
 func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CategoryRequest) (*pb.CategoryResponse, error) {
-	categoryID, err := c.CategoryDB.Create(in.Name, in.Description)
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
 	if err != nil {
 		return nil, err
 	}
-
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
 }

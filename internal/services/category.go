91fb79d80a735de98d0a73ea7438b48399ad1edb
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-18 12:28:07 -0300

    workiding stream

--- internal/services/category.go
+++ internal/services/category.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"io"
 
 	"github.com/VituSuperMEg/go-grpc-fullcycle/internal/database"
 	"github.com/VituSuperMEg/go-grpc-fullcycle/internal/pb"
@@ -65,4 +66,28 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 	}
 
 	return categoryResponse, nil
+}
+
+// Stream
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err != io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
